b7f93fa29273879c01237ccb0aadb30ef101504d
Roman Shatalov <rshatalov@gmail.com>
2025-08-09 20:32:06 +0300

    Add gitignore and backend

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/{id:str}/")
+async def get_id(id: str):
+    return {"id": id}
+
+
+@app.get("/activities/{slug:str}/")
+async def activity(slug: str):
+    return {"slug": slug}
